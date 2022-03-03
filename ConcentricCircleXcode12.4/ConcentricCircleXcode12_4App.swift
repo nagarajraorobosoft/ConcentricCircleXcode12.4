da78126ead45605764355b4b3ec60430abc7553d
Nagaraj Rao <nagaraj.rao@robosoftin.com>
2022-03-03 11:50:02 +0530

    Initial Commit

---
+++ ConcentricCircleXcode12.4/ConcentricCircleXcode12_4App.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentricCircleXcode12_4App.swift
+//  ConcentricCircleXcode12.4
+//
+//  Created by Nagaraj on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcentricCircleXcode12_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
